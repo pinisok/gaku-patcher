0a60cc209d5c177265e4ad218168c3f24e9fd080
pinisok <prain3ps@gmail.com>
2026-09-19 11:43:58 +0900

    fix(clone): use the Korean game name for the launcher label
    Constraint: Clone package is com.bandainamcoent.idolmaster_gakuen_kr
    
    Confidence: high
    
    Scope-risk: narrow
    
    Not-tested: Launcher label in a newly rebuilt APK

--- clone.sh
+++ clone.sh
@@ -5,12 +5,12 @@ OLD_PACKAGE_NAME="com.bandainamcoent.idolmaster_gakuen"
 NEW_PACKAGE_NAME="com.bandainamcoent.idolmaster_gakuen_kr"
 
 GAKUMAS_JP="学マス"
-GAKUMAS_EN="Gakumas"
+GAKUMAS_KR="학원 아이돌마스터"
 NAME_FILE="./$APK_EXTRACT_DIR/resources/package_1/res/values/strings.xml"
 
 java -jar APKEditor.jar d -t xml -dex -i "$GAME_APK_NAME" -o $APK_EXTRACT_DIR
 
 grep -rIl "$OLD_PACKAGE_NAME" ./$APK_EXTRACT_DIR | xargs sed -i "s/$OLD_PACKAGE_NAME\>/$NEW_PACKAGE_NAME/g"
-sed -i "s/$GAKUMAS_JP/$GAKUMAS_EN/g" $NAME_FILE
+sed -i "s/$GAKUMAS_JP/$GAKUMAS_KR/g" $NAME_FILE
 
 java -jar APKEditor.jar b -i $APK_EXTRACT_DIR -o "$GAME_CLONED_NAME"
